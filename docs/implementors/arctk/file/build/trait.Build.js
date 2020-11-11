c679d52654b4dd5a390329821999ef16af9c1c97
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-11 22:17:15 +0000

    Added documentation.

---
+++ docs/implementors/arctk/file/build/trait.Build.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["arctk"] = [];
+implementors["babbage"] = [{"text":"impl Build for OperationBuilder","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
